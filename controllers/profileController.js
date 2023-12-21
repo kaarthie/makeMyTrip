539f046fd3bd7040a0fe1e67d636bda6bca86847
kaarthie <karthironaldo989@gmail.com>
2023-12-21 16:56:33 +0530

    ProfileEdit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,29 +1,25 @@
 const User = require('../models/userModel.js')
 
 module.exports.profileEdit = async (req, res) => {
-    const {email} = req.body;
-  console.log("jeu")
-    try {
-      // Find the user by ID
-      const user = await User.findOne({email});
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      // Update only non-empty and non-space values in the request body
-      for (const key in req.body) {
-        if (key in user._doc && req.body[key].trim() !== '') {
-          user[key] = req.body[key].trim();
-        }
-      }
-  
-      // Save the updated user
-      await user.save();
-  
-      res.json({ message: 'User updated successfully', user });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  const { email } = req.body;
+
+  try {
+    // Find the user by email using findOne
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
+
+    // Update user fields based on values from req.body
+    Object.assign(user, req.body);
+
+    // Update the user using updateOne
+    await User.updateOne({ email: user.email }, user);
+
+    res.json({ message: 'User updated successfully', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};
